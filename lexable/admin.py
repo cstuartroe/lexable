ee02a093d5d0f8e25732887e42b4776f1b36d77b
Conor Stuart-Roe <conorstuartroe@gmail.com>
2026-01-25 21:54:39 -0500

    Register models on admin

--- lexable/admin.py
+++ lexable/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from lexable.models import derived, document, lexeme, purchases, sentence, trial, user_adds, user_settings, word, wordlist
+
+
+admin.site.register(derived.LexemeForm)
+admin.site.register(derived.UserHistoryDate)
+admin.site.register(derived.CardDueDate)
+admin.site.register(derived.CreditBalance)
+
+admin.site.register(document.Collection)
+admin.site.register(document.CollectionTitleTranslation)
+admin.site.register(document.Document)
+admin.site.register(document.DocumentTitleTranslation)
+admin.site.register(document.Section)
+
+admin.site.register(lexeme.Lexeme)
+admin.site.register(lexeme.Etymology)
+admin.site.register(lexeme.LexemeSense)
+admin.site.register(lexeme.Definition)
+
+admin.site.register(purchases.CreditPurchase)
+admin.site.register(purchases.DocumentUnlock)
+
+admin.site.register(sentence.Sentence)
+admin.site.register(sentence.SentenceTranslation)
+
+admin.site.register(trial.Trial)
+
+admin.site.register(user_adds.LexemeAdd)
+admin.site.register(user_adds.SectionAdd)
+
+admin.site.register(user_settings.UserSettings)
+
+admin.site.register(word.WordInSentence)
+admin.site.register(word.Substring)
+
+admin.site.register(wordlist.WordList)
+admin.site.register(wordlist.WordListItem)
